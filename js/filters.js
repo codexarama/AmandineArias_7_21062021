cb5c793a39bd99995779d4b9d3879be2e54e61d7
Amandine Arias <amandine.arias@gmail.com>
2021-06-18 15:08:10 +0200

    fix (filters) : search events improvements

--- js/filters.js
+++ js/filters.js
@@ -1,39 +1,32 @@
 // HANDLE FILTER BOXES EVENTS
 // DOM ELEMENTS
 const searchBox = document.querySelectorAll('.search-box');
-const filtersBtns = document.querySelectorAll('.search-nav-btn');
 // OPEN / CLOSE FILTER BOXES
 // add / remouve attributes
 for (let i = 0; i < searchBox.length; i++) {
   searchBox[i].addEventListener('click', (event) => {
     event.preventDefault();
     searchBox[i].classList.toggle('open');
-    searchBox[i].firstElementChild.style.width = '40rem'
-    const searchItem = searchBox[i].firstElementChild.firstElementChild.firstElementChild;
+    const searchBtn = searchBox[i].firstElementChild;
+    searchBtn.style.width = '40rem';
+    const searchItem = searchBtn.children[1];
     searchItem.style.display = 'block';
     searchItem.setAttribute(
       'placeholder',
-      'Rechercher des ' + searchBox[i].firstElementChild.textContent.trim()
+      'Rechercher des ' + searchBtn.textContent.toLowerCase().trim()
     );
-
-    // searchBox[i].firstElementChild.textContent = ' ';
-
-    // for (let j = 0; j < filtersBtns.length; j++) {
-    //   filtersBtns[j].textContent = ''
-    // }
-
-    // btns styles on events
-    // for (let j = 0; j < filtersBtns.length; j++) {
-    //   filtersBtns[j].style.borderRadius = '0.25rem 0.25rem 0 0';
-    // }
-    // or
-    //     filtersBtns.forEach(
-    //       (btn) => (btn.style.borderRadius = '0.25rem 0.25rem 0 0')
-    //     );
+    searchBtn.children[0].style.display = 'none';
   });
 
   // close filter boxes without target
   window.addEventListener('click', function (e) {
-    if (!searchBox[i].contains(e.target)) searchBox[i].classList.remove('open');
+    if (!searchBox[i].contains(e.target)) {
+      searchBox[i].classList.remove('open');
+      const searchBtn = searchBox[i].firstElementChild;
+      searchBtn.children[0].style.display = 'flex';
+      searchBtn.style.width = '8.25rem';
+      const searchItem = searchBtn.children[1];
+      searchItem.style.display = 'none';
+    }
   });
 }
