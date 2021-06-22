ef6d83937acd1bd09417deaa4850fa364b4ffdf3
Amandine Arias <amandine.arias@gmail.com>
2021-06-22 14:16:41 +0200

    feat (filters) : add tag type input events

--- js/tags.js
+++ js/tags.js
@@ -1,32 +1,57 @@
-// HANDLE FILTER BOXES EVENTS
-// DOM ELEMENTS
-const searchBox = document.querySelectorAll('.search-box');
-// OPEN / CLOSE FILTER BOXES
-// add / remouve attributes
-for (let i = 0; i < searchBox.length; i++) {
-  searchBox[i].addEventListener('click', (event) => {
+// TYPE TEXT IN TAG INPUT
+// MAIN DOM ELEMENT
+const tagLabel = document.querySelectorAll('.tag-label');
+// DEPENDANT DOM ELEMENTS
+tagLabel.forEach((label) => {
+  const tagInput = label.parentNode.children[1];
+  // HANDLE TEXT INPUT ACCESS
+  label.addEventListener('click', (event) => {
     event.preventDefault();
-    searchBox[i].classList.toggle('open');
-    const searchBtn = searchBox[i].firstElementChild;
-    searchBtn.style.width = '40rem';
-    const searchItem = searchBtn.children[1];
-    searchItem.style.display = 'block';
-    searchItem.setAttribute(
-      'placeholder',
-      'Rechercher des ' + searchBtn.textContent.toLowerCase().trim()
-    );
-    searchBtn.children[0].style.display = 'none';
-  });
+    console.log(label);
+    console.log(tagInput);
+    label.style.display = 'none'
+    tagInput.setAttribute('placeholder', 'Rechercher')
+    tagInput.style.display = 'block'
+    tagInput.style.width = '5rem'
+  })
+})
 
-  // close filter boxes without target
+// OPEN / CLOSE FILTER BOXES by clicking on the arrow
+// MAIN DOM ELEMENT
+const tagArrow = document.querySelectorAll('.tag-arrow');
+// DEPENDANT DOM ELEMENTS
+tagArrow.forEach((arrow) => {
+  const tagBox = arrow.parentNode.parentNode;
+  const tagBtn = arrow.parentNode;
+  const tagLabel = tagBtn.children[0];
+  const searchTag = tagBtn.children[1];
+  const tagsList = tagBox.children[1];
+  // HANDLE OPEN FILTER BOXES EVENTS
+  arrow.addEventListener('click', (event) => {
+    event.preventDefault();
+    tagBox.classList.toggle('open');
+    tagBtn.style.width = '40rem';
+    tagLabel.style.display = 'none';
+    searchTag.setAttribute(
+      'placeholder',
+      'Rechercher des ' + tagBtn.textContent.toLowerCase().trim()
+    );
+    searchTag.style.display = 'block';
+    tagsList.style.display = 'block';
+  });
+  // CLOSE FILTER BOXES WITHOUT TARGET
   window.addEventListener('click', function (e) {
-    if (!searchBox[i].contains(e.target)) {
-      searchBox[i].classList.remove('open');
-      const searchBtn = searchBox[i].firstElementChild;
-      searchBtn.children[0].style.display = 'flex';
-      searchBtn.style.width = '8.25rem';
-      const searchItem = searchBtn.children[1];
-      searchItem.style.display = 'none';
+    if (!arrow.contains(e.target)) {
+      tagBox.classList.remove('open');
+      tagLabel.style.display = 'flex';
+      tagBtn.style.width = '8.25rem';
+      searchTag.style.display = 'none';
+      tagsList.style.display = 'none';
+    }
+    if(tagLabel.contains(e.target)) {
+      tagLabel.style.display = 'none';
+      searchTag.style.display = 'flex';
+      searchTag.focus();
     }
   });
-}
+});
