f1b44f8729b11e2eae2897c019c14bdc13119475
Amandine Arias <amandine.arias@gmail.com>
2021-07-05 15:57:19 +0200

    feat (style) : improvements linked to events

--- js/tags.js
+++ js/tags.js
@@ -1,10 +1,13 @@
-// TYPE TEXT IN TAG INPUT
+// TAGS EVENTS
 // ingredients, appareils, ustensiles
+
 // MAIN DOM ELEMENT
 const tagLabel = document.querySelectorAll('.tag-label');
+
 // DEPENDANT DOM ELEMENTS
 tagLabel.forEach((label) => {
   const tagInput = label.parentNode.children[1];
+
   // HANDLE TEXT INPUT ACCESS
   label.addEventListener('click', (event) => {
     event.preventDefault();
@@ -12,12 +15,14 @@ tagLabel.forEach((label) => {
     tagInput.setAttribute('placeholder', 'Rechercher');
     tagInput.style.display = 'block';
     tagInput.style.width = '5rem';
+    tagBtn.style.borderRadius = '0.25rem 0.25rem 0 0'
   });
 });
 
 // OPEN / CLOSE FILTER BOXES by clicking on the arrow
 // MAIN DOM ELEMENT
 const tagArrow = document.querySelectorAll('.tag-arrow');
+
 // DEPENDANT DOM ELEMENTS
 tagArrow.forEach((arrow) => {
   const tagBox = arrow.parentNode.parentNode;
@@ -25,20 +30,31 @@ tagArrow.forEach((arrow) => {
   const tagLabel = tagBtn.children[0];
   const searchTag = tagBtn.children[1];
   const tagsList = tagBox.children[1];
+
   // HANDLE OPEN FILTER BOXES EVENTS
   arrow.addEventListener('click', (event) => {
     event.preventDefault();
     tagBox.classList.toggle('open');
     tagBtn.style.width = '40rem';
     tagLabel.style.display = 'none';
+
     searchTag.setAttribute(
       'placeholder',
       'Rechercher des ' + tagBtn.textContent.toLowerCase().trim()
     );
+
     searchTag.style.display = 'block';
     searchTag.focus();
     tagsList.style.display = 'flex';
+
+    if (!tagBox.classList.contains('open')) {
+      searchTag.style.display = 'none';
+      tagBtn.style.width = '8.25rem';
+      tagsList.style.display = 'none';
+      tagLabel.style.display = 'inline-block';
+    }
   });
+
   // CLOSE FILTER BOXES WITHOUT TARGET
   window.addEventListener('click', function (e) {
     if (!arrow.contains(e.target)) {
@@ -47,11 +63,15 @@ tagArrow.forEach((arrow) => {
       tagBtn.style.width = '8.25rem';
       searchTag.style.display = 'none';
       tagsList.style.display = 'none';
+      tagBtn.style.borderRadius = '0.25rem'
     }
+
     if (tagLabel.contains(e.target)) {
       tagLabel.style.display = 'none';
       searchTag.style.display = 'flex';
       searchTag.focus();
+      tagBtn.style.borderRadius = '0.25rem 0.25rem 0 0'
+      tagsList.style.borderRadius = '0 0.25rem 0.25rem 0.25rem'
     }
   });
 });
