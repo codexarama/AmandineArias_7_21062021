ea94183751babe1ab3a05dbcf1c2a7c28a3e4cc6
Amandine Arias <amandine.arias@gmail.com>
2021-07-05 15:56:15 +0200

    refactor (docs) : add spaces between lines of code

--- js/search_result.js
+++ js/search_result.js
@@ -1,12 +1,13 @@
-// FONCTION
-// RESULTATS RECHERCHE (from main search bar)
+// RESULTATS RECHERCHE
 // detecte et montre correspondances
-function searchRecipe(input, list, options) {
+function lookForCorrespondance(input, list, options) {
   input.addEventListener('keyup', () => {
-    filter = input.value.toUpperCase();
-    const textValue = options.textContent || options.innerText;
+    const searchInput = input.value.toUpperCase();
+    // const searchInput = input.value.normalize("NFCK");
+    const textValue = options.textContent;
+
     // if (textValue.length > 3 && textValue.toUpperCase().indexOf(filter) > -1) {}
-    if (textValue.toUpperCase().indexOf(filter) > -1) {
+    if (textValue.toUpperCase().indexOf(searchInput) > -1) {
       list.style.display = 'flex';
       options.style.display = '';
       options.classList.add('matches');
@@ -17,26 +18,32 @@ function searchRecipe(input, list, options) {
   });
 }
 
-// FONCTION
 // AFFICHE SELECTION(S)
 // DOM element
 const recipeSection = document.querySelector('#recipes');
+
 // cree tableau [choix]
 const choices = [];
 function displaySelection(event) {
   const selected = event.target;
+
   // retire "selected" du choix precedent
   if (selected.classList.contains('selected'))
     selected.classList.remove('selected');
+
   // affecte "selected" au nouveau choix
   selected.classList.add('selected');
+
   // ajoute au tableau [choix] (sans doublons)
   if (!choices.includes(selected)) {
     choices.push(selected);
+
     // cree tag(s) correspondant(s)
     createTag(selected);
+
     // detecte dernier choix
     const lastSelection = document.querySelector('#tags-collection').lastChild;
+
     // attribue code couleur selon categorie
     if (selected.classList.contains('recipe-option'))
       lastSelection.classList.add('recipes-result-btn');
@@ -47,6 +54,7 @@ function displaySelection(event) {
     if (selected.classList.contains('ustensils-option'))
       lastSelection.classList.add('ustensils-result-btn');
   }
+
   // -----------------------------------------------------------------------------
   console.log(choices);
   // -----------------------------------------------------------------------------
@@ -55,12 +63,15 @@ function displaySelection(event) {
     .then((response) => response.json())
     .then((data) => {
       const recipes = data.recipes;
+
       // cree tableau(x) [recette(s) a afficher]
       let recipesToDisplay = [];
+
       // ajoute recette(s) correspondant au(x) choix
       choices.forEach((choice) => {
         recipesToDisplay.push(getRecipesByChoice(recipes, choice.textContent));
       });
+
       // fusionne tableaux [recette(s) a afficher]
       function merge(recipesToDisplay) {
         return recipesToDisplay.reduce(
@@ -68,8 +79,10 @@ function displaySelection(event) {
           []
         );
       }
+
       // supprime doublons
       const uniqueRecipe = [...new Set(merge(recipesToDisplay))];
+
       // affiche recettes correspondant au(x) choix
       for (let i = 0; i < uniqueRecipe.length; i++) {
         setRecipe(uniqueRecipe[i]);
@@ -85,6 +98,7 @@ function displaySelection(event) {
         // ---------------------------------------------------------------------
       }
     });
+
   // recupere recettes correspondant au(x) choix
   function getRecipesByChoice(recipes, choice) {
     recipesByChoice = [];
@@ -99,6 +113,7 @@ function displaySelection(event) {
         }
       });
       console.log(choice);
+
       if (
         recipe.name === choice ||
         recipe.description === choice ||
@@ -109,15 +124,17 @@ function displaySelection(event) {
         recipesByChoice.push(recipe);
       }
     });
+
     return recipesByChoice;
   }
+
   // supprime cartes recettes deja affichees
   recipeSection.innerHTML = '';
 }
 
-// FONCTION
 // AFFICHE MESSAGE SI AUCUN CRITERE DE RECHERCHE NE CORRESPOND
 createAlert();
+
 function checkMatches() {
   const searchMatches = document.querySelectorAll('.matches');
   const alert = document.querySelector('.alert-msg');
